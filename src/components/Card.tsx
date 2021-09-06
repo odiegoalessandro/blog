6632c238e843382970c0131209dd36328e857564
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-09-05 22:56:13 -0300

    fix a bug

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Text, useColorModeValue, HStack } from "@chakra-ui/react"
+import { Flex, Heading, Text, HStack, DarkMode, LightMode } from "@chakra-ui/react"
 
 export default function Card({ post }){
   let title: string = post.metadata.title
@@ -7,37 +7,76 @@ export default function Card({ post }){
   }
 
   return (
-    <Flex
-      bgColor={useColorModeValue("gray.300", "gray.700")}
-      borderRadius="md"
-      w="45rem"
-      h="6rem"
-      justifyContent="space-between"
-      p="5"
-      flexDir="column"
-    >
-      <Heading fontSize="2xl">{title}</Heading>
-      <HStack spacing="2">
-        {
-          post.metadata.tags.map(tag => {
-            return (
-              <Text 
-                bgColor={useColorModeValue('gray.200', 'gray.600')}
-                borderRadius="10px"
-                align="center"
-                px="2"
-                key={tag}
-                minW="2rem"
-                _hover={{
-                  bgColor: useColorModeValue("var(--chakra-colors-gray-100)", "var(--chakra-colors-gray-500)")
-                }}
-              >
-                {tag}
-              </Text>
-            )
-          })
-        }
-      </HStack>
-    </Flex>
+    <>
+      <DarkMode>
+        <Flex
+          bgColor="gray.700"
+          borderRadius="md"
+          w="45rem"
+          h="6rem"
+          justifyContent="space-between"
+          p="5"
+          flexDir="column"
+        >
+        <Heading fontSize="2xl">{title}</Heading>
+          <HStack spacing="2">
+            {
+              post.metadata.tags.map(tag => {
+                return (
+                  <Text 
+                    bgColor="gray.600"
+                    borderRadius="10px"
+                    align="center"
+                    px="2"
+                    key={tag}
+                    minW="2rem"
+                    _hover={{
+                      bgColor: "var(--chakra-colors-gray-500)"
+                    }}
+                  >
+                    {tag}
+                  </Text>
+                )
+              })
+            }
+          </HStack>
+        </Flex>
+      </DarkMode>
+      <LightMode>
+        <Flex
+          bgColor="gray.300"
+          borderRadius="md"
+          w="45rem"
+          h="6rem"
+          justifyContent="space-between"
+          p="5"
+          flexDir="column"
+        >
+        <Heading fontSize="2xl">{title}</Heading>
+          <HStack spacing="2">
+            {
+              post.metadata.tags.map(tag => {
+                return (
+                  <Text 
+                    bgColor="gray.200"
+                    borderRadius="10px"
+                    align="center"
+                    px="2"
+                    key={tag}
+                    minW="2rem"
+                    _hover={{
+                      bgColor: "var(--chakra-colors-gray-100)"
+                    }}
+                  >
+                    {tag}
+                  </Text>
+                )
+              })
+            }
+          </HStack>
+        </Flex>
+      </LightMode>
+    </>
+    
   )
 }
