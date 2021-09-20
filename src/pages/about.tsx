8407d47486ef5961c7414c3c38194351c4bf71cd
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-09-20 19:51:29 -0300

    master

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,33 @@
-import { Center, Box, Heading, VStack, Text } from "@chakra-ui/react"
+import { Center, Box, Heading, VStack, Text, HeadingProps, TextProps, CenterProps } from "@chakra-ui/react"
 import Tooltip from "../components/Tooltip"
 import Head from 'next/head'
+import { motion, Variants } from 'framer-motion'
+
+
+const MotionHeading = motion<HeadingProps>(Heading)
+const MotionText = motion<TextProps>(Text)
+const MotionCenter = motion<CenterProps>(Center)
+
+const elementVariants: Variants = {
+  hidden: {opacity: 0},
+  visible: {opacity: 1}
+}
+
+const variants: Variants = {
+  hidden: {
+    y: "50px",
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+      duration: .5,
+      staggerChildren: .4
+    }
+  }
+}
 
 export default function About(){
   
@@ -9,7 +36,7 @@ export default function About(){
       <Head>
         <title>Sobre mim</title>
       </Head>
-      <Center 
+      <MotionCenter 
         flexDir="column"
         px={{
           base: "2rem",
@@ -18,9 +45,12 @@ export default function About(){
           lg: "15rem",
           xl: "20rem"
         }}
+        initial="hidden"
+        animate="visible"
+        variants={variants}
       >
         <Box>
-          <Heading
+          <MotionHeading
             marginTop="8rem"
             fontSize={{
               xl: "5xl", 
@@ -29,10 +59,11 @@ export default function About(){
               sm: "5xl", 
               base: "3xl"
             }}
+            variants={elementVariants}
           >
             Sobre
-          </Heading>
-          <Heading 
+          </MotionHeading>
+          <MotionHeading 
             fontSize={{
               xl: "5xl", 
               lg: "5xl", 
@@ -41,30 +72,32 @@ export default function About(){
               base: "3xl"
             }}
             color="#ff0080"
+            variants={elementVariants}
           >
             Diego Alessandro
-          </Heading>
+          </MotionHeading>
           <VStack spacing="1rem" marginTop="1rem">
-            <Text as="p">
+            <MotionText as="p" variants={elementVariants}>
               Olá me chamo Diego e tenho 15 anos. Eu comecei a estudar desenvolvimento web em 2019,
               durante a pandemia fiz parte de varios eventos sobre os fundamentos basicos do desenvolvimento
               web especialmente desenvolvimento front-end.
-            </Text>
-            <Text as="p">
+            </MotionText>
+            <MotionText variants={elementVariants} as="p">
               Existem milhões de tecnologias para aprender no front-end e meu objetivo é dominar o maximo
               de tecnologias possivel. Gosto muito de aprender coisas novas e estou disposto a receber
               criticas para aprefeiçoar meus conhecimentos.
-            </Text>
-            <Text as="p">
+            </MotionText>
+            <MotionText variants={elementVariants} as="p">
               Neste site ainda não tenho um objetivo final, porém pretendo colocar dicas, side-projects, artigos,
               meus projetos em destaque e coisas do tipo para que possa armazenar e compartilhar meu conhecimento
               ao longo da minha jornada como desenvolvedor front-end. Então entre em contato comigo para que
               possamos evoluir juntos!
-            </Text>
+            </MotionText>
           </VStack>
-          <Heading
+          <MotionHeading
             as="h2"
             marginTop="4rem"
+            variants={elementVariants}
             fontSize={{
               xl: "3xl", 
               lg: "3xl", 
@@ -74,16 +107,21 @@ export default function About(){
             }}
           >
             Contato
-          </Heading>
-          <Text as="p" marginTop="1rem" marginBottom="4rem">
+          </MotionHeading>
+          <MotionText 
+            variants={elementVariants}
+            as="p"
+            marginTop="1rem"
+            marginBottom="4rem"
+          >
             Entre em contanto comigo se precisar de uma opinião em algum projeto ou coisa parecida especialmente
             em projetos front-end ou open-source. Fico feliz em ajudar! Entre em contaoto por esse {" "}
             <Tooltip label="diegoalessandrodacruzmartins@gmail.com">
               <b>email</b>
             </Tooltip>
-          </Text>
+          </MotionText>
         </Box>
-      </Center>
+      </MotionCenter>
     </>
   )
 }
