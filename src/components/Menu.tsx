d4ad4d6f3edd5471222fa63bcac9c03aa2ed0cec
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-09-21 11:12:25 -0300

    improving accessibility in menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Heading, Flex, useColorModeValue, Link, HStack } from "@chakra-ui/react";
+import { IconButton, Flex, Link, useColorModeValue } from "@chakra-ui/react";
 import { useColorMode } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
@@ -10,52 +10,55 @@ export default function Menu(){
       align="center"
       justify="space-between"
       h="5rem"
-      role="menu"
-      px={{base: "2rem", sm: "5rem", md: "10rem", lg: "15rem", xl: "20rem"}}
+      px={{
+        base: "2rem",
+        sm: "5rem",
+        md: "10rem",
+        lg: "15rem",
+        xl: "20rem"
+      }}
       bgColor={useColorModeValue("gray.300", "gray.700")}
       pos="fixed"
       zIndex="10"
       inset="0"
+      as="header"
+      role="navigation"
     > 
-      <Heading 
+      <Link
+        href="/"
+        transition=".3s"
+        pos="relative"
+        fontWeight="bold"
         fontSize="1.2rem"
-        role="heading"
+        _hover={{
+          color: "#ff0080",
+          _after: {
+            w: "100%",
+          }
+        }}
+        _after={{
+          content: "''",
+          height: "2px",
+          bgColor: "#FF0080",
+          position: "absolute",
+          left: "0",
+          bottom: "-3px",
+          transition: ".3s",
+          w: "0"
+        }}
+        role="link"
       >
-        <Link
-          role="link"
-          href="/"
-          transition=".3s"
-          pos="relative"
-          _hover={{
-            color: "#ff0080",
-            _after: {
-              w: "100%",
-            }
-          }}
-          _after={{
-            content: "''",
-            height: "2px",
-            bgColor: "#FF0080",
-            position: "absolute",
-            left: "0",
-            bottom: "-3px",
-            transition: ".3s",
-            w: "0"
-          }}
-        >
-          Diego alessandro
-        </Link>
-      </Heading>
+        Diego alessandro
+      </Link>
       <IconButton
-        aria-label="Theme"
-        role="button"
+        aria-label="Mudar o tema"
         bgColor="transparent"
         borderRadius="50%"
         onClick={toggleColorMode}
         w="10"
         fontSize="large"
         icon={
-          colorMode === "light" ? (<MoonIcon role="img" />) : (<SunIcon role="img" />)
+          colorMode === "light" ? (<MoonIcon aria-label="Tema escuro" />) : (<SunIcon aria-label="Tema claro" />)
         }
     />
     </Flex>
