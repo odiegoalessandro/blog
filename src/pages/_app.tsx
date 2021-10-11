831155757cd803890dba7c723bc139822558c2b0
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-10-11 14:13:45 -0300

    using comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,18 @@ import theme from "../themes/theme"
 import GlobalStyles from '../styles/GlobalStyles'
 import Menu from "../components/Menu"
 import Footer from "../components/Footer"
+import { AuthContextProvider } from "../contexts/AuthContext"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <GlobalStyles />
-      <Menu />
-      <Component {...pageProps} />
-      <Footer />   
-    </ChakraProvider>
+    <AuthContextProvider>
+      <ChakraProvider theme={theme}>
+        <GlobalStyles />
+        <Menu />
+        <Component {...pageProps} />
+        <Footer />   
+      </ChakraProvider>
+    </AuthContextProvider>
   )
 }
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,9 +1,12 @@
 import { getAllSlugs, getPostBySlug } from "../../utils/posts"
 import { GetStaticPaths, GetStaticProps } from "next"
-import { Flex, Center, Heading, HStack, Text, Image } from "@chakra-ui/react"
+import { Flex, Center, Heading, HStack, Text, Image, Button, Avatar, Input } from "@chakra-ui/react"
 import Head from 'next/head'
 import React from "react"
 import Markdown from "../../components/Markdown"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGoogle } from "@fortawesome/free-brands-svg-icons"
+import useAuth from "../../hook/useAuth"
 
 interface SlugProps {
   data: {
@@ -20,6 +23,7 @@ interface SlugProps {
 }
 
 const Slug: React.FC<SlugProps> = ({ data }) => {
+  const { signInWithGoogle, user } = useAuth()
   const months = [
     "Jan",
     "Fev",
@@ -38,6 +42,7 @@ const Slug: React.FC<SlugProps> = ({ data }) => {
   const date = new Date(data.metadata.date)
   const postingDate = `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}`
 
+  console.log(user)
   return (
     <>
       <Head>
@@ -92,6 +97,56 @@ const Slug: React.FC<SlugProps> = ({ data }) => {
             alt={data.metadata.title}
           />
           <Markdown content={data.content} />
+          <Flex
+            marginTop="3rem"
+            flexDir="column"
+          >
+            <Heading fontSize="xl">
+              Comentários
+            </Heading>
+            {
+              user ? (
+                <HStack marginTop="1rem">
+                  <Avatar src={user.avatar} borderRadius="8px" w="40px" h="40px" />
+                  <Input placeholder="Iniciar discussão..." />
+                </HStack>
+              ) : (
+                <>    
+                  <Button 
+                    marginTop="1rem"
+                    maxW="350px"
+                    h="50px"
+                    display="flex"
+                    fontSize={{
+                      xl: "17px",
+                      lg: "17px",
+                      md: "17px",
+                      sm: "17px",
+                      base: "13px"
+                    }}
+                    justifyContent="space-between"
+                    p={{
+                      xl: "0 3rem",
+                      lg: "0 3rem",
+                      md: "0 3rem",
+                      sm: "0 3rem",
+                      base: "0 1rem"
+                    }}
+                    backgroundColor="red.600"
+                    transition="filter .2s"
+                    _hover={{
+                      filter: "brightness(.7)"
+                    }}
+                    color="#fff"
+                    onClick={signInWithGoogle}
+                  >
+                    <FontAwesomeIcon size="lg" icon={faGoogle} />
+                    Faça login para comentar
+                  </Button>
+                </>
+              )
+            }
+          </Flex>
         </Flex>
       </Center>
     </>
