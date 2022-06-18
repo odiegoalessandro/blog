536e549aae0aad70c3d9934c22aeca859665346c
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2022-06-17 22:35:52 -0300

    finished demo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,12 +9,12 @@ export default function About() {
         <NextImage
           width="1000px"
           height="400px"
-          src="photo-1446071103084-c257b5f70672?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=684&q=80"
+          src="https://images.unsplash.com/photo-1446071103084-c257b5f70672?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=684&q=80"
           alt="plants"
           objectFit="cover"
           style={{
             borderRadius: "15px",
-            filter: "grayscale(100%)"
+            filter: "grayscale(100%)",
           }}
           loading="eager"
         />
@@ -28,24 +28,24 @@ export default function About() {
           independente e sempre aberto a criticas ou novas ideias.
         </Text>
         <Text as="p" mt="2rem" color="whiteAlpha.600">
-          Meu interesse começou em um dia quando estava na escola e um colega meu
-          veio e me disse "meu pai me colocou em um curso de programação e é super
-          legal", sinceramente eu nem sabia oque era programação mas fiquei uma
-          certa inveja do meu amigo saber coisas que eu nem fazia ideia de que
-          existiam, nesse mesmo dia eu cheguei em casa e fui pesquisar "como
-          aprender a programar", acabei caindo em um video chamado "Curso de
-          Python 01: Seja um programador" obviamente assisti a esse video e fiquei
-          maravilhado com tudo aquilo e eu sendo curioso do jeito que sou acessei
-          aquele canal no youtube e vi todo conteudo de python que eles tinham(na
-          epoca era cerca de uns 4 videos) após isso em um almoço de domingo
-          qualquer eu cheguei na mesa todo orgulhoso e disse "eu fiz uma
-          calculadora querem ver?", e depois desse dia eu nunca mais parei desde
-          dos meus 11 anos até agora.
+          Meu interesse começou em um dia quando estava na escola e um colega
+          meu veio e me disse "meu pai me colocou em um curso de programação e é
+          super legal", sinceramente eu nem sabia oque era programação mas
+          fiquei uma certa inveja do meu amigo saber coisas que eu nem fazia
+          ideia de que existiam, nesse mesmo dia eu cheguei em casa e fui
+          pesquisar "como aprender a programar", acabei caindo em um video
+          chamado "Curso de Python 01: Seja um programador" obviamente assisti a
+          esse video e fiquei maravilhado com tudo aquilo e eu sendo curioso do
+          jeito que sou acessei aquele canal no youtube e vi todo conteudo de
+          python que eles tinham(na epoca era cerca de uns 4 videos) após isso
+          em um almoço de domingo qualquer eu cheguei na mesa todo orgulhoso e
+          disse "eu fiz uma calculadora querem ver?", e depois desse dia eu
+          nunca mais parei desde dos meus 11 anos até agora.
         </Text>
         <Text as="p" mt="2rem" color="whiteAlpha.600">
           Por algum tempo meu sonho foi ser desenvolvedor de games, mas por não
-          ter como investir em bons conteudos e não possuir um bom computador foi
-          uma ideia abandonada.
+          ter como investir em bons conteudos e não possuir um bom computador
+          foi uma ideia abandonada.
         </Text>
         <Text as="p" mt="2rem" color="whiteAlpha.600">
           Atualmente penso em finalizar a escola, iniciar uma falculdade em
@@ -57,13 +57,13 @@ export default function About() {
         </Heading>
         <UnorderedList color="whiteAlpha.600" spacing="1rem" mb="2rem">
           <ListItem>
-            Comecei a aprender programação em um celular, na epoca um motorola g3
-            e śo fui ter meu primeiro computador cerca de 2 anos depois.
+            Comecei a aprender programação em um celular, na epoca um motorola
+            g3 e śo fui ter meu primeiro computador cerca de 2 anos depois.
           </ListItem>
           <ListItem>
-            Nunca fiz um curso pago ou presencial, meus maiores professores foram
-            alguns devs proximos de mim e cursos gratuitos como "next level week",
-            "imersão alura" e derivados.
+            Nunca fiz um curso pago ou presencial, meus maiores professores
+            foram alguns devs proximos de mim e cursos gratuitos como "next
+            level week", "imersão alura" e derivados.
           </ListItem>
           <ListItem>
             Meu primeiro projeto foi um "hello world" quando eu tinha 11 anos de
@@ -71,8 +71,8 @@ export default function About() {
           </ListItem>
           <ListItem>Eu escuto musica 24 horas por dia.</ListItem>
           <ListItem>
-            Sempre tive muito interesse em artes marciais já tendo praticado judô,
-            jiu-jitsu e atualmente pratico muay thai.
+            Sempre tive muito interesse em artes marciais já tendo praticado
+            judô, jiu-jitsu e atualmente pratico muay thai.
           </ListItem>
         </UnorderedList>
       </Box>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,10 @@
-import { Flex, Heading, LinkBox, LinkOverlay } from "@chakra-ui/react"
+import {
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react"
 import { GetStaticProps } from "next"
 import Layout from "../../components/layout"
 import { getAllPosts } from "../../lib/mdx"
@@ -10,19 +16,50 @@ interface PostsProps {
 }
 
 export default function Posts({ posts }: PostsProps) {
-  console.log(posts)
-
   return (
     <Layout title="posts">
-      <Flex mt="3.5rem">
-        {posts.map(post => (
-          <LinkBox as="article">
-            <Heading>
-              <LinkOverlay href={`posts/${post.slug}`}>{post.title}</LinkOverlay>
-            </Heading>
-          </LinkBox>
-        ))}
-      </Flex>
+      <Heading mt="5rem">
+        Postagens
+      </Heading>
+      <SimpleGrid pt="2rem" columns={2} spacing={10} minH="100vh">
+        {posts.map(post => {
+          const excerpt =
+            post.excerpt.length > 200
+              ? post.excerpt.substring(0, 200) + "..."
+              : post.excerpt
+
+          return (
+            <LinkBox
+              as="article"
+              border="1px solid"
+              borderColor="whiteAlpha.600"
+              p=".8rem 1.5rem"
+              h="14rem"
+              borderRadius="md"
+              transition=".3s"
+              _hover={{
+                borderColor: "white",
+              }}
+            >
+              <Heading
+                size="lg"
+                whiteSpace="nowrap"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                transition=".3s"
+                _hover={{ color: "whiteAlpha.800" }}
+              >
+                <LinkOverlay href={`posts/${post.slug}`}>
+                  {post.title}
+                </LinkOverlay>
+              </Heading>
+              <Text mt="1.5rem" color="whiteAlpha.600">
+                {excerpt}
+              </Text>
+            </LinkBox>
+          )
+        })}
+      </SimpleGrid>
     </Layout>
   )
 }
